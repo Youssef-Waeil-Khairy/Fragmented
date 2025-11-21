4e1cd4544298ff1d9f59be95cd1eaf130cfa690f
JanWillem Goedvolk <caringsharing57@gmail.com>
2025-11-21 21:06:32 +0400

    Character and Camera Controls Implemented

---
+++ AGENTS.md
@@ -0,0 +1,8 @@
+<!-- UNITY CODE ASSIST INSTRUCTIONS START -->
+- Project name: Fragmented
+- Unity version: Unity 6000.1.14f1
+- Active game object:
+  - Name: Room
+  - Tag: Untagged
+  - Layer: Default
+<!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Assets/Scripts/PlayerControls/PlayerController.cs
+++ Assets/Scripts/PlayerControls/PlayerController.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -17,11 +18,16 @@ namespace PlayerControls
         [SerializeField] float moveDirection = 0f;
         [SerializeField] float turnDirection = 0f;
         [SerializeField] bool isSprinting = false;
-        
+
+        [Header("Camera")]
+        [SerializeField] private CinemachineInputAxisController cinemachineInputAxisController;
+        [SerializeField] private bool cursorFree = false;
+
         private void Start()
         {
             playerInput = GetComponent<PlayerInput>();
             characterController = GetComponent<CharacterController>();
+            LockCursor();
         }
 
         private void FixedUpdate()
@@ -29,6 +35,43 @@ namespace PlayerControls
             Vector3 move = transform.forward * (moveDirection * (isSprinting ? sprintSpeed : moveSpeed));
             characterController.SimpleMove(move);
             transform.Rotate(transform.up, turnDirection *  turnSpeed * Time.fixedDeltaTime);
+        }
+
+        void ToggleCursorFree()
+        {
+            cursorFree = !cursorFree;
+
+            if (cursorFree)
+            {
+                UnlockCursor();
+            }
+            else
+            {
+                LockCursor();
+            }
+        }
+
+        void LockCursor()
+        {
+            cursorFree = false;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            cinemachineInputAxisController.enabled = true;
+        }
+        void UnlockCursor()
+        {
+            cursorFree = true;
+            Cursor.lockState = CursorLockMode.Confined;
+            Cursor.visible = true;
+            cinemachineInputAxisController.enabled = false;
+        }
+
+        void OnFreeCursor(InputValue value)
+        {
+            if (value.isPressed)
+            {
+                ToggleCursorFree();
+            }
         }
 
         void OnMove(InputValue value)
