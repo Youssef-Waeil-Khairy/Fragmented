a422e4aa3d0443df498f9bf2117f9803cc38e552
JanWillem Goedvolk <caringsharing57@gmail.com>
2026-03-26 02:30:26 +0400

    Screen Transition Added
    Not yet implemented in the levels

--- AGENTS.md
+++ AGENTS.md
@@ -2,13 +2,13 @@
 - Project name: Fragmented
 - Unity version: Unity 6000.1.14f1
 - Active scene:
-  - Name: Tutorial_Home
+  - Name: ScreenTransition
   - Tags:
     - Untagged, Respawn, Finish, EditorOnly, MainCamera, Player, GameController, Command, EchoMina, Interactable, Stair
   - Layers:
     - Default, TransparentFX, Ignore Raycast, Ground, Water, UI, Interactible, EchoMina, Walls, Player, Stairs
 - Active game object:
-  - Name: Upper Floor Floor
+  - Name: ScreenTransition
   - Tag: Untagged
-  - Layer: Default
+  - Layer: UI
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
---
+++ Assets/Scripts/MainMenu/ScreenTransitioner.cs
@@ -0,0 +1,69 @@
+using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MainMenu
+{
+    public class ScreenTransitioner : MonoBehaviour
+    {
+        public enum TransitionType
+        {
+            FadeIn,
+            FadeOut,
+            None
+        }
+
+        public Image TransitionImage;
+        [CurveRange(0, 0, 1, 1)] public AnimationCurve TransitionCurveIn;
+        [CurveRange(0, 0, 1, 1)]public AnimationCurve TransitionCurveOut;
+        public float FadeInTime, FadeOutTime;
+        public float FadeTime;
+        public TransitionType Transition = TransitionType.FadeOut;
+
+        private void Update()
+        {
+            switch (Transition)
+            {
+                case TransitionType.FadeIn:
+                    FadeTime += Time.deltaTime;
+                    float timeIn = FadeTime / FadeInTime;
+                    float alphaIn = TransitionCurveIn.Evaluate(timeIn);
+                    TransitionImage.color = new Color(1, 1, 1, alphaIn);
+                    if (FadeTime >= FadeInTime)
+                    {
+                        Transition = TransitionType.None;
+                        FadeTime = 0f;
+                    }
+                    break;
+                case TransitionType.FadeOut:
+                    FadeTime += Time.deltaTime;
+                    float timeOut = FadeTime / FadeOutTime;
+                    float alphaOut = TransitionCurveOut.Evaluate(timeOut);
+                    TransitionImage.color = new Color(1, 1, 1, alphaOut);
+                    if (FadeTime >= FadeOutTime)
+                    {
+                        Transition = TransitionType.None;
+                        FadeTime = 0f;
+                    }
+                    break;
+                case TransitionType.None:
+                    break;
+            }
+        }
+
+        [Button]
+        public void FadeIn()
+        {
+            Transition = TransitionType.FadeIn;
+            FadeTime = 0f;
+        }
+
+        [Button]
+        public void FadeOut()
+        {
+            Transition = TransitionType.FadeOut;
+            FadeTime = 0f;
+        }
+
+    }
+}
