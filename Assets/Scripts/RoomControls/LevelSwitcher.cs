1655508ada1d5adf678a9934a498fe80b90bfc77
JanWillem Goedvolk <caringsharing57@gmail.com>
2026-04-30 20:15:00 +0400

    Fixed Settings Scene Leaving

--- Assets/Scripts/RoomControls/LevelSwitcher.cs
+++ Assets/Scripts/RoomControls/LevelSwitcher.cs
@@ -1,6 +1,7 @@
 using JetBrains.Annotations;
 using UnityEngine;
 using NaughtyAttributes;
+using PlayerControls;
 using UnityEngine.SceneManagement;
 
 namespace RoomControls
@@ -10,6 +11,9 @@ namespace RoomControls
         [Scene][SerializeField] private int level;
         [Tag][SerializeField] private string allowedTag;
         public bool IsInterLevelTransition;
+
+        public bool IsLeavingSettingsScene = false;
+        public bool IsGoingToSettingsScene = false;
 
         private void OnTriggerEnter(Collider other)
         {
@@ -22,7 +26,31 @@ namespace RoomControls
         [Button][UsedImplicitly]
         public void GoToScene()
         {
+            Debug.Log($"Loading scene: {level} from {SceneManager.GetActiveScene().buildIndex}");
+
             SceneManager.LoadScene(level);
+            PauseMenu.Instance.CurrentScene = level;
+        }
+
+        public void GoToSettingsScene()
+        {
+            Debug.Log("level switcher going to settings scene");
+            PauseMenu.Instance.CurrentScene = SceneManager.GetActiveScene().buildIndex;
+            PauseMenu.Instance.TakeSnapshot();
+            Debug.Log($"Loading scene: {level} from {SceneManager.GetActiveScene().buildIndex}");
+            SceneManager.LoadScene(level);
+        }
+
+        public void LeaveSettingsScene()
+        {
+            Debug.Log($"Loading scene: {PauseMenu.Instance.CurrentScene} from {SceneManager.GetActiveScene().buildIndex}");
+            SceneManager.LoadScene(PauseMenu.Instance.CurrentScene);
+
+            while (!SceneManager.GetActiveScene().isLoaded)
+            {
+                Debug.Log("Waiting for scene to load");
+            }
+            PauseMenu.Instance.LoadSnapshot();
         }
 
         public void CloseGame()
